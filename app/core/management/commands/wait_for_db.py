f9bc27fab3b0e117e716ffb14c777b9f28606ff1
ashishkr619 <ashishkrhhh@gmail.com>
2020-08-16 23:18:58 +0530

    Added manangement command to core app

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -1,14 +1,13 @@
 import time
 from django.db import connections
 from django.db.utils import OperationalError
-from django.core.management.base import BaseCommand,CommandError
-
+from django.core.management.base import BaseCommand
 
 
 class Command(BaseCommand):
-    help ='Django command to pause execution until database is available'
+    help = 'Django command to pause execution until database is available'
 
-    def handle(self,*args,**options):
+    def handle(self, *args, **options):
         self.stdout.write('Waiting for database')
         db_conn = None
 
@@ -16,8 +15,8 @@ class Command(BaseCommand):
             try:
                 db_conn = connections['default']
             except OperationalError:
-                self.stdout.write('Database unavailable waiting for 1 second...')
+                self.stdout.write(
+                    'Database unavailable waiting for 1 second...')
                 time.sleep(1)
 
-        self.stdout.write(self.style.SUCCESS('Database available!'))            
-
+        self.stdout.write(self.style.SUCCESS('Database available!'))
